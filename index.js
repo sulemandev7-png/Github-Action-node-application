d6a669d82ff59b808bfbee749e7b4536053de379
sulemandev7-png <suleman.dev7@gmail.com>
2026-06-01 16:44:54 +0500

    Adding code

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from your simple Node.js application!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
